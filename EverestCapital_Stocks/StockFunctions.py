2371d7ce3b089d9cfba06acb70d67f1714678ff1
anishmoktan <anish.tmngo@gmail.com>
2020-11-18 14:50:47 -0500

    calculations

--- EverestCapital_Stocks/StockFunctions.py
+++ EverestCapital_Stocks/StockFunctions.py
@@ -16,6 +16,29 @@ class Stock_Functions:
         self.columns = ["Cash", "Portfolio_Raw", "Portfolio_Updated", "ROI", "Total Networth"]   # providing values for the colmuns
         self.table = self.DB.Table(self.__Tablename__)
 
+    def initiate_account(username):
+        response = self.table.put_item(
+            Item={
+                self.Primary_Column_Name: username,
+                self.columns[0]: 10000,
+                self.columns[1]: {},
+                self.columns[2]: {},
+                self.columns[3]: 0,
+                self.columns[4]: 10000
+            }
+        if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
+            return {
+                "Result": True,
+                "Error": None,
+                "Description": "User's stock DB was created succesfully",
+            }
+        else:
+            return  {
+                "Result": False,
+                "Error": "Database error",
+                "Description": "Database error",
+            }
+
     def stock_price(stock_symbol):
         stock_symbol = stock_symbol.upper()
         base_url  = "https://www.alphavantage.co/query?"
@@ -39,14 +62,21 @@ class Stock_Functions:
         if stock_query[Message] == "Successful":
 
 
-        
+    def sell_stock(stock_symbol):
+        stock_query = self.stock_price(stock_symbol)
+        if stock_query[Message] == "Successful":
+
         #   if response.status_code == 200:
         #       print('\nWe were able to find the value of the stock!')
         #   elif response.status_code == 404:
         #       print('Sorry we could not find the stock you were looking for')
         #       return(self.search_stock)
-
     
-      
-      
-  
+
+
+    #For solving calculations:
+    #  port= { 'AAPL':{"2020-11-17":[2,150],"2020-11-16":[3,160]} }
+
+    # access=port['AAPL']
+    # for i in access:
+    # print(access[i][0]*access[i][1])
